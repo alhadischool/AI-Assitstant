687654d224004ba763d661936750967d56339953
alhadi school <al.hadi.qom@gmail.com>
2026-08-29 11:31:20 +0330

    change model

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def load_llm():
         
     return ChatGroq(
         api_key=MY_GROQ_KEY,
-        model_name="llama3-70b-8192",
+        model_name="grok-4.1-fast",
         temperature=0
     )
 
